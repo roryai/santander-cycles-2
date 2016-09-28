a19d27fc4694ad9200cdc5fc7ece7e1db7053a9a
elizabethvenner <elizabethvenner@googlemail.com>
2016-09-28 16:09:49 +0100

    Add bike_release method to docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-
-
+  describe 'bike_release' do
+   it 'releases the bike' do
+     expect (DockingStation.new).respond_to? (:bike_release)
+    end
+  end
 end

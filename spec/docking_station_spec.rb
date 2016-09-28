47cf64a57185ac21ea4da7a9a649abc1067c4e87
elizabethvenner <elizabethvenner@googlemail.com>
2016-09-28 16:20:34 +0100

    Modify bike_release test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  describe 'bike_release' do
    it 'releases the bike' do
-     expect (DockingStation.new).respond_to? (:bike_release)
-    end
+     expect(subject).to respond_to :bike_release
   end
 end

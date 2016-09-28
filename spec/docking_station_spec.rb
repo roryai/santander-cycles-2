d5eb34b07547031719894f2b810faddd30e16ebc
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-28 18:57:36 +0100

    added unit test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do
-   it {is_expected.to respond_to :release_bike}
+	
+   	it {is_expected.to respond_to :release_bike} 
+	
+
+	describe 'create new instance of bike class using release_bike' do
+    it {is_expected.to :release_bike}
+	end
 
 end
+

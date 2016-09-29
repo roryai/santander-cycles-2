275c1e5e1f03011ad30d5f16135b6a7c22955a31
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-29 12:09:48 +0100

    added Bike.new to docking_station; working method to bike class; wrote test for aforementioned

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
-	
-   	it {is_expected.to respond_to :release_bike} 
-	
+		describe 'responds to release_bike'
+   	it {is_expected.to respond_to :release_bike}
 
-	describe 'create new instance of bike class using release_bike' do
-    it {is_expected.to :release_bike}
-	end
+#		describe 'is bike working?'
+#    it {is_expected.to }
 
 end
-

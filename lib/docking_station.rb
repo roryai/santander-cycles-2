8872bbf1c903adf43c2482abe63e644d94a4b796
Rory Aitchison <rory.aitchison@gmail.com>
2016-09-29 15:07:47 +0100

    exercise 11 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,4 +6,17 @@ class DockingStation
 		Bike.new
 	end
 
+	def dock_bike(bike)
+		@bike = bike
+	end
+
+	attr_reader :bike
+	# line above equals 3 lines below
+	# def bike
+	# 	@bike
+	# end
+
 end
+
+
+# station.dock_bike(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,10 @@ describe DockingStation do
 		describe 'responds to release_bike'
    	it {is_expected.to respond_to :release_bike}
 
-#		describe 'is bike working?'
-#    it {is_expected.to }
+		describe 'responds to docks_bike'
+		it {is_expected.to respond_to :dock_bike}
+
+		describe 'docks a bike'
+		it {is_expected.to respond_to(:dock_bike).with(1).argument}
 
 end
